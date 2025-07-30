b73cb384da0cd52e3686fd4e04ecefa6ef6509b4
Troy Lorents <tjlorents333@gmail.com>
2025-07-30 01:06:58 -0700

    contact page oublickey update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -59,7 +59,7 @@ const Contact = () => {
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
         process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
         refForm.current,
-        process.env.REACT_APP_EMAILJS_USER_ID
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
